35f18a516a8d562d9032d283211afe06849204d2
Jason Liang <jjjasonliang@gmail.com>
2018-06-14 16:44:55 +0900

    complete graph

--- dataset3.py
+++ dataset3.py
@@ -11,4 +11,17 @@ growth_rate = growth_rate.reshape(-1, 1)
 
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = "NaN", strategy = "mean")
-growth_rate = imputer.fit_transform(growth_rate)        
+growth_rate = imputer.fit_transform(growth_rate)
+
+
+fig, ax1 = plt.subplots()
+ax1.set_xlabel("Year")
+ax1.set_ylabel("Grants")
+ax1.bar(year, grants, label = "Grants", color = 'b', alpha = 0.2)
+plt.legend(bbox_to_anchor=(0.22, 0.9))
+
+ax2 = ax1.twinx()
+ax2.set_ylabel("Growth Rate")
+ax2.plot(year, growth_rate, label = "Growth Rate", color='green')
+plt.legend(bbox_to_anchor=(0.3, 1))
+plt.show()
