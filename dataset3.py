9c882bf5a4120452b8f22bc04216f6b88ee3e6f8
Jason Liang <jjjasonliang@gmail.com>
2018-06-14 16:00:40 +0900

    preprocess data

--- dataset3.py
+++ dataset3.py
@@ -3,3 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 dataset3 = pd.read_excel("wipo_pub_941_2015-tech1/A03.xlsx", header = 5, skeprows = 4)
+year = dataset3.iloc[:, 0].values
+grants = dataset3.iloc[:, 1].values
+growth_rate = dataset3.iloc[:, 2].values
+growth_rate = pd.to_numeric(growth_rate, errors = 'coerce')
+
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = '.', strategy = 'mean')
+growth_rate = imputer.fit_transform(growth_rate)
